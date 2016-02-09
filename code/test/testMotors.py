62041ba3d556deb5a09b5ec6c2de3bb3cdf23edc
Danixk <dadric9@gmail.com>
2016-02-09 16:36:57 +0000

    Structure 1

--- code/test/testMotors.py
+++ code/test/testMotors.py
@@ -1,3 +1,17 @@
+# IMPORT MODULES FROM SUBFOLDERS #
+""" It's neccesary in order to import modules not in the same folder, but in a different one.
+This is the way to tell python the location on those subfolders: """
+import os, sys, inspect
+
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
+
+sys.path.append('../')
+# ------------------------------ #
+
+
+
 from tortoise import Tortoise
 
 def main():
--- code/tortoise.py
+++ code/tortoise.py
@@ -1,3 +1,18 @@
+# IMPORT MODULES FROM SUBFOLDERS #
+""" It's neccesary in order to import modules not in the same folder, but in a different one.
+This is the way to tell python the location on those subfolders: """
+import os, sys, inspect
+
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
+
+# Subfolder "lowlevel"
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"lowlevel")))
+if cmd_subfolder not in sys.path:
+	sys.path.insert(0, cmd_subfolder)
+# ------------------------------ #
+
 from motors import Motor
 
 class Tortoise:
