397cb8201f4fb25534fc6d9a731a9731ae96ada3
Danixk <dadric9@gmail.com>
2016-02-23 23:56:29 +0000

    added bluePeter

--- code/activity.py
+++ code/activity.py
@@ -1,20 +1,19 @@
 from tortoise import Tortoise
 from enums import Direction, SensorType
 
-
 def main():
-	Mike = Tortoise()
-
+	Bob=Tortoise()
 	while True:
-		if Mike.getSensorData(SensorType.light, 1) < 6:
-			Mike.doRandomStep()
-			print "Where's the light?"
-		elif 6<= Mike.getSensorData(SensorType.light, 1)  and Mike.getSensorData(SensorType.light, 1) <9:
-			print "Found Light"
-			Mike.moveMotors(30, Direction.forward)	
-		else: 
-			print "Argh! Too much light!"
-			Mike.moveMotors(30, Direction.backward)
+		if Bob.getSensorData(SensorType.light,1)<4:
+			Bob.doRandomStep()
+			print "where are you?"
+		elif Bob.getSensorData(SensorType.light,1)<9:
+			Bob.moveMotors(30,Direction.forward)
+			print "ah-ha!"
+		else :
+			Bob.moveMotors(30,Direction.backward)
+			print "Burning burning!"
 
-if __name__=="__main__":
+if __name__ =="__main__":
 	main()
+
