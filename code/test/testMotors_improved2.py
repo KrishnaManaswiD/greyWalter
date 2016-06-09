7a701416a280bf5804c04d618615a551b3a2cd63
Daniel Carrillo Zapata <daniel2.carrillozapata@live.uwe.ac.uk>
2016-06-09 16:41:24 +0100

    Two improvements

---
+++ code/test/testMotors_improved2.py
@@ -0,0 +1,29 @@
+# IMPORT MODULES FROM SUBFOLDERS #
+""" It's neccesary in order to import modules not in the same folder, but in a different one.
+This is the way to tell python the location on those subfolders: """
+import os, sys, inspect
+
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
+
+sys.path.append('../')
+# ------------------------------ #
+
+from tortoise import Tortoise
+import enums
+import time
+
+def main():
+        wanderer = Tortoise()
+
+	while True:
+
+        wanderer.moveMotors_improved2(500, enums.Direction.forward)
+        time.sleep(1)
+        wanderer.moveMotors_improved2(500, enums.Direction.backward)
+        time.sleep(1)
+        
+
+if __name__ == "__main__":
+	main()
