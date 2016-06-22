0fde6a924b26b1cfe14488d795fbe6c18e2bc768
Daniel Carrillo Zapata <daniel2.carrillozapata@live.uwe.ac.uk>
2016-06-22 16:33:38 +0200

    Improved setup_tortoise

--- code/setup_tortoise.sh
+++ code/setup_tortoise.sh
@@ -7,13 +7,21 @@
 # at startup time.
 ####################################################
 
-sudo apt-get install -y git
 
-sudo apt-get install -y ntpdate
+echo "Installing ntpdate"
+sudo apt-get install -y ntpdate >>/dev/null
+echo "DONE"
+echo
 
-sudo apt-get install -y gedit
+echo "Installing gedit"
+sudo apt-get install -y gedit >>/dev/null
+echo "DONE"
+echo
 
-sudo pip install enum34
+echo "Installing enum34 (python module)"
+sudo pip install enum34 >>/dev/null
+echo "DONE"
+echo
 
 # Directory of this file
 BASEDIR=$(dirname `readlink -f $0`)
@@ -23,14 +31,18 @@ chmod +x $BASEDIR/lowlevel/watchdog.sh
 # If the watchdog is not in the .bashrc file, it is added
 if ! grep -Fxq "$BASEDIR/lowlevel/watchdog.sh >/dev/null &" ~/.bashrc
 then
-        echo  >> ~/.bashrc
-        echo "# Only one instance of the weatchdog is created" >> ~/.bashrc
-        echo "if [ \`ps aux | grep watchdog.sh | wc -l\` -eq 1 ]" >> ~/.bashrc
-        echo "then" >> ~/.bashrc
-	echo -e "\t$BASEDIR/lowlevel/watchdog.sh >/dev/null &" >> ~/.bashrc
-        echo "fi" >> ~/.bashrc
+    echo "Updating ~/.bashrc"
+
+    echo  >> ~/.bashrc
+    echo "# Only one instance of the weatchdog is created" >> ~/.bashrc
+    echo "if [ \`ps aux | grep watchdog.sh | wc -l\` -eq 1 ]" >> ~/.bashrc
+    echo "then" >> ~/.bashrc
+    echo -e "\t$BASEDIR/lowlevel/watchdog.sh >/dev/null &" >> ~/.bashrc
+    echo "fi" >> ~/.bashrc
 
 	source ~/.bashrc
+
+    echo "DONE"
 fi
 
 exit 0
