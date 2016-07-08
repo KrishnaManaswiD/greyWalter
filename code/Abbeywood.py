d0fa2c6525153fa2f78e2a37d01463307ee02296
Chanelle-work <chanelle1202@googlemail.com>
2016-07-08 18:05:22 +0100

    Chanelle: Updated abbeywood code (haven't run to test)

--- code/Abbeywood.py
+++ code/Abbeywood.py
@@ -1,15 +1,15 @@
 from tortoise import Tortoise
-from enums import Direction, SensorType, ActuatorType
+from enums import Direction, SensorType
 
 Name=Tortoise()
 
 while True:
 
-    touchSensor = Name.getSensorData(SensorType.touch,1)
-    if touchSensor == 1:
-        print "Switch is on"
-        Name.setLEDValue(1, 1) #self.setLEDValue(position, value)
-	Name.moveForwards(50)
+	touchSensorValue = Name.getSensorData(SensorType.touch,1)
+    if touchSensorValue == 1:
+        print "Touch sensor is being pressed"
+        Name.setLEDValue(1, 1)
+		Name.moveForwards(50)
     else:
-        print "Switch is off"
-        Name.setLEDValue(1, 0) #self.setLEDValue(position, value)
+        print "Nothing is pressing the touch sensor"
+        Name.setLEDValue(1, 0)
