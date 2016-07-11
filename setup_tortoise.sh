e3d7d626537c327bac19ffaa3327bd540ad1ddc1
Danixk <dadric9@gmail.com>
2016-07-11 16:19:15 +0100

    Changed setup_tortoise and merged basic documentation

--- setup_tortoise.sh
+++ setup_tortoise.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-####################################################
+############################################################################
 # This file needs to be run when setting up the tortoise for the first time
 #
 # The watchdog background process is added to .bashrc to be launched
 # at startup time.
-####################################################
+############################################################################
 
 
 echo "Installing ntpdate"
@@ -26,10 +26,10 @@ echo
 # Directory of this file
 BASEDIR=$(dirname `readlink -f $0`)
 
-chmod +x $BASEDIR/lowlevel/watchdog.sh
+chmod +x $BASEDIR/code/lowlevel/watchdog.sh
 
 # If the watchdog is not in the .bashrc file, it is added
-if ! grep -Fxq "$BASEDIR/lowlevel/watchdog.sh >/dev/null &" ~/.bashrc
+if ! grep -Fxq "$BASEDIR/code/lowlevel/watchdog.sh >/dev/null &" ~/.bashrc
 then
     echo "Updating ~/.bashrc"
 
@@ -37,7 +37,7 @@ then
     echo "# Only one instance of the weatchdog is created" >> ~/.bashrc
     echo "if [ \`ps aux | grep watchdog.sh | wc -l\` -eq 1 ]" >> ~/.bashrc
     echo "then" >> ~/.bashrc
-    echo -e "\t$BASEDIR/lowlevel/watchdog.sh >/dev/null &" >> ~/.bashrc
+    echo -e "\t$BASEDIR/code/lowlevel/watchdog.sh >/dev/null &" >> ~/.bashrc
     echo "fi" >> ~/.bashrc
 
 	source ~/.bashrc
